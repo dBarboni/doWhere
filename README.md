e4ff1e08b0dddd4351208ef0698832fd7a620dd9
Unknown <dan@barboni.net>
2018-04-06 20:59:02 -0400

    Add update and delete routes

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-doWhere
+Based on [this](https://medium.freecodecamp.org/building-a-simple-node-js-api-in-under-30-minutes-a07ea9e390d2) tutorial.
--- app/routes/todo_routes.js
+++ app/routes/todo_routes.js
@@ -1,12 +1,12 @@
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db){
+  //Get task
   app.get('/tasks/:id', (req, res) => {
-    // Get Task
     const id = req.params.id;
     const details = {'_id': new ObjectID(id)};
     db.collection('tasks').findOne(details, (err, item) => {
-      if (err){
+      if(err){
         res.send({'error':'An error has occurred'});
       }
       else{
@@ -14,15 +14,42 @@ module.exports = function(app, db){
       }
     });
   });
-
+  //Create task
   app.post('/tasks', (req, res) => {
-    // Create task
-    const task = { text: req.body.body, title: req.body.title };
+    const task = {text: req.body.body, title: req.body.title };
     db.collection('tasks').insert(task, (err, result) => {
-      if (err) {
+      if(err){
         res.send({ 'error': 'An error has occurred' });
-      } else {
+      }
+      else{
         res.send(result.ops[0]);
+      }
+    });
+  });
+  //Delete task
+  app.delete('/tasks/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('tasks').remove(details, (err, item) => {
+      if(err){
+        res.send({'error':'An error has occurred'});
+      }
+      else{
+        res.send('Task ' + id + ' deleted!');
+      }
+    });
+  });
+  //Update task
+  app.put('/tasks/:id', (req, res) => {
+    const id = req.params.id;
+    const details = {'_id': new ObjectID(id) };
+    const task = {text: req.body.body, title: req.body.title};
+    db.collection('tasks').update(details, task, (err, result) => {
+      if(err){
+          res.send({'error':'An error has occurred'});
+      }
+      else{
+          res.send(task);
       }
     });
   });
