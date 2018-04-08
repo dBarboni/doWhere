1dc4397d12979612f5643db0eea7f4a7d197effb
Unknown <dan@barboni.net>
2018-04-08 12:08:34 -0400

    Add methods to get all tasks, unique rooms, unique users

--- app/routes/todo_routes.js
+++ app/routes/todo_routes.js
@@ -1,13 +1,25 @@
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db){
-  //Get task
+  //Get all tasks
+  app.get('/tasks', (req, res) => {
+    db.collection("tasks").find().toArray(function(err, result) {
+      if(err){
+        res.send({'error': err});
+      }
+      else{
+        res.send(result);
+      }
+    });
+  });
+
+  //Get task by id
   app.get('/tasks/:id', (req, res) => {
     const id = req.params.id;
     const details = {'_id': new ObjectID(id)};
     db.collection('tasks').findOne(details, (err, item) => {
       if(err){
-        res.send({'error':'An error has occurred'});
+        res.send({'error': err});
       }
       else{
         res.send(item);
@@ -15,12 +27,36 @@ module.exports = function(app, db){
     });
   });
 
+  //Get unique rooms
+  app.get('/rooms', (req, res) => {
+    db.collection("tasks").distinct("room", (function(err, result) {
+      if(err){
+        res.send({'error': err});
+      }
+      else{
+        res.send(result);
+      }
+    }));
+  });
+
+  //Get unique users
+  app.get('/users', (req, res) => {
+    db.collection("tasks").distinct("user", (function(err, result) {
+      if(err){
+        res.send({'error': err});
+      }
+      else{
+        res.send(result);
+      }
+    }));
+  });
+
   //Get tasks by room
   app.get('/rooms/:room', (req, res) => {
     const room = req.params.room;
     db.collection("tasks").find({room: room}).toArray(function(err, result) {
       if(err){
-        res.send({'error':'An error has occurred'});
+        res.send({'error': err});
       }
       else{
         res.send(result);
@@ -30,10 +66,10 @@ module.exports = function(app, db){
 
   //Create task
   app.post('/tasks', (req, res) => {
-    const item = {room: req.body.room, task: req.body.task};
+    const item = {room: req.body.room, task: req.body.task, user: req.body.user};
     db.collection('tasks').insert(item, (err, result) => {
       if(err){
-        res.send({ 'error': 'An error has occurred'});
+        res.send({ 'error': err});
       }
       else{
         res.send(result.ops[0]);
@@ -41,13 +77,13 @@ module.exports = function(app, db){
     });
   });
 
-  //Delete task
+  //Delete task by id
   app.delete('/tasks/:id', (req, res) => {
     const id = req.params.id;
     const details = {'_id': new ObjectID(id)};
     db.collection('tasks').remove(details, (err, item) => {
       if(err){
-        res.send({'error':'An error has occurred'});
+        res.send({'error': err});
       }
       else{
         res.send('Task ' + id + ' deleted!');
@@ -55,17 +91,17 @@ module.exports = function(app, db){
     });
   });
 
-  //Update task
+  //Update task by id
   app.put('/tasks/:id', (req, res) => {
     const id = req.params.id;
     const details = {'_id': new ObjectID(id)};
-    const item = {room: req.body.room, task: req.body.task};
+    const item = {room: req.body.room, task: req.body.task, user: req.body.user};
     db.collection('tasks').update(details, item, (err, result) => {
       if(err){
-          res.send({'error':'An error has occurred'});
+          res.send({'error': err});
       }
       else{
-          res.send(task);
+          res.send(item);
       }
     });
   });
